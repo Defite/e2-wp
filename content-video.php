e44b0927124d2e08f2e6ec31fffe1558074f755e
Nikita Makhov <defite@gmail.com>
2013-05-09 19:07:59 +0400

    added video post format support

---
+++ content-video.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying posts in the Video post format.
+ *
+ * @package WordPress
+ * @subpackage e2
+ * @since e2 1.0
+ */ 
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<h1 class="entry-title"><?php the_title(); ?></h1>
+                <?php edit_post_link( __( 'Edit', 'e2' ), '<span class="edit-link">', '</span>' ); ?>
+		<span class="entry-meta">
+			<?php e2_posted_on(); ?>
+		</span><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-media">
+		<?php the_post_format_video(); ?>
+	</div><!-- .entry-media -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+        
+		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'e2' ), 'after' => '</div>' ) ); ?>
+	</div><!-- .entry-content -->
+
+</article><!-- #post-<?php the_ID(); ?> -->
