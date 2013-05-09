c92c8e19e1934730649b5cac8a1db5d6596e58ab
Nikita Makhov <defite@gmail.com>
2013-05-09 19:05:41 +0400

    added video post format support

--- single.php
+++ single.php
@@ -15,7 +15,7 @@ get_header(); ?>
 
 				<?php e2_content_nav( 'nav-above' ); ?>
 
-				<?php get_template_part( 'content', 'single' ); ?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
 
 				<?php e2_content_nav( 'nav-below' ); ?>
 
